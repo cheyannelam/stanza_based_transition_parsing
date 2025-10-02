4f2cf7c100bbc418a238d91307b1dafcccb288be
Cheyanne <cheyannelhy@gmail.com>
2025-10-02 15:05:00 -0700

    removed useless structure

--- stanza/models/constituency/transition_sequence.py
+++ stanza/models/constituency/transition_sequence.py
@@ -7,7 +7,8 @@ Supports multiple transition schemes - TOP_DOWN and variants, IN_ORDER
 import logging
 
 from stanza.models.common import utils
-from stanza.models.constituency.parse_transitions import Shift, CompoundUnary, OpenConstituent, CloseConstituent, TransitionScheme, Finalize
+#from stanza.models.constituency.parse_transitions import Shift, CompoundUnary, OpenConstituent, CloseConstituent, TransitionScheme, Finalize
+from stanza.models.constituency.parse_transitions import Shift, LeftArc, RightArc, TransitionScheme
 from stanza.models.constituency.tree_reader import read_trees
 from stanza.utils.get_tqdm import get_tqdm
 
@@ -15,46 +16,46 @@ tqdm = get_tqdm()
 
 logger = logging.getLogger('stanza.constituency.trainer')
 
-def yield_top_down_sequence(tree, transition_scheme=TransitionScheme.TOP_DOWN_UNARY):
-    """
-    For tree (X A B C D), yield Open(X) A B C D Close
+# def yield_top_down_sequence(tree, transition_scheme=TransitionScheme.TOP_DOWN_UNARY):
+#     """
+#     For tree (X A B C D), yield Open(X) A B C D Close
 
-    The details are in how to treat unary transitions
-    Three possibilities handled by this method:
-      TOP_DOWN_UNARY:    (Y (X ...)) -> Open(X) ... Close Unary(Y)
-      TOP_DOWN_COMPOUND: (Y (X ...)) -> Open(Y, X) ... Close
-      TOP_DOWN:          (Y (X ...)) -> Open(Y) Open(X) ... Close Close
-    """
-    if tree.is_preterminal():
-        yield Shift()
-        return
+#     The details are in how to treat unary transitions
+#     Three possibilities handled by this method:
+#       TOP_DOWN_UNARY:    (Y (X ...)) -> Open(X) ... Close Unary(Y)
+#       TOP_DOWN_COMPOUND: (Y (X ...)) -> Open(Y, X) ... Close
+#       TOP_DOWN:          (Y (X ...)) -> Open(Y) Open(X) ... Close Close
+#     """
+#     if tree.is_preterminal():
+#         yield Shift()
+#         return
 
-    if tree.is_leaf():
-        return
+#     if tree.is_leaf():
+#         return
 
-    if transition_scheme is TransitionScheme.TOP_DOWN_UNARY:
-        if len(tree.children) == 1:
-            labels = []
-            while not tree.is_preterminal() and len(tree.children) == 1:
-                labels.append(tree.label)
-                tree = tree.children[0]
-            for transition in yield_top_down_sequence(tree, transition_scheme):
-                yield transition
-            yield CompoundUnary(*labels)
-            return
+#     if transition_scheme is TransitionScheme.TOP_DOWN_UNARY:
+#         if len(tree.children) == 1:
+#             labels = []
+#             while not tree.is_preterminal() and len(tree.children) == 1:
+#                 labels.append(tree.label)
+#                 tree = tree.children[0]
+#             for transition in yield_top_down_sequence(tree, transition_scheme):
+#                 yield transition
+#             yield CompoundUnary(*labels)
+#             return
 
-    if transition_scheme is TransitionScheme.TOP_DOWN_COMPOUND:
-        labels = [tree.label]
-        while len(tree.children) == 1 and not tree.children[0].is_preterminal():
-            tree = tree.children[0]
-            labels.append(tree.label)
-        yield OpenConstituent(*labels)
-    else:
-        yield OpenConstituent(tree.label)
-    for child in tree.children:
-        for transition in yield_top_down_sequence(child, transition_scheme):
-            yield transition
-    yield CloseConstituent()
+#     if transition_scheme is TransitionScheme.TOP_DOWN_COMPOUND:
+#         labels = [tree.label]
+#         while len(tree.children) == 1 and not tree.children[0].is_preterminal():
+#             tree = tree.children[0]
+#             labels.append(tree.label)
+#         yield OpenConstituent(*labels)
+#     else:
+#         yield OpenConstituent(tree.label)
+#     for child in tree.children:
+#         for transition in yield_top_down_sequence(child, transition_scheme):
+#             yield transition
+#     yield CloseConstituent()
 
 def yield_in_order_sequence(tree):
     """
@@ -80,49 +81,49 @@ def yield_in_order_sequence(tree):
 
 
 
-def yield_in_order_compound_sequence(tree, transition_scheme):
-    def helper(tree):
-        if tree.is_leaf():
-            return
+# def yield_in_order_compound_sequence(tree, transition_scheme):
+#     def helper(tree):
+#         if tree.is_leaf():
+#             return
 
-        labels = []
-        while len(tree.children) == 1 and not tree.is_preterminal():
-            labels.append(tree.label)
-            tree = tree.children[0]
+#         labels = []
+#         while len(tree.children) == 1 and not tree.is_preterminal():
+#             labels.append(tree.label)
+#             tree = tree.children[0]
 
-        if tree.is_preterminal():
-            yield Shift()
-            if len(labels) > 0:
-                yield CompoundUnary(*labels)
-            return
+#         if tree.is_preterminal():
+#             yield Shift()
+#             if len(labels) > 0:
+#                 yield CompoundUnary(*labels)
+#             return
 
-        for transition in helper(tree.children[0]):
-            yield transition
+#         for transition in helper(tree.children[0]):
+#             yield transition
 
-        if transition_scheme is TransitionScheme.IN_ORDER_UNARY:
-            yield OpenConstituent(tree.label)
-        else:
-            labels.append(tree.label)
-            yield OpenConstituent(*labels)
+#         if transition_scheme is TransitionScheme.IN_ORDER_UNARY:
+#             yield OpenConstituent(tree.label)
+#         else:
+#             labels.append(tree.label)
+#             yield OpenConstituent(*labels)
 
-        for child in tree.children[1:]:
-            for transition in helper(child):
-                yield transition
+#         for child in tree.children[1:]:
+#             for transition in helper(child):
+#                 yield transition
 
-        yield CloseConstituent()
+#         yield CloseConstituent()
 
-        if transition_scheme is TransitionScheme.IN_ORDER_UNARY and len(labels) > 0:
-            yield CompoundUnary(*labels)
+#         if transition_scheme is TransitionScheme.IN_ORDER_UNARY and len(labels) > 0:
+#             yield CompoundUnary(*labels)
 
-    if len(tree.children) == 0:
-        raise ValueError("Cannot build {} on an empty tree".format(transition_scheme))
-    if len(tree.children) != 1:
-        raise ValueError("Cannot build {} with a tree that has two top level nodes: {}".format(transition_scheme, tree))
+#     if len(tree.children) == 0:
+#         raise ValueError("Cannot build {} on an empty tree".format(transition_scheme))
+#     if len(tree.children) != 1:
+#         raise ValueError("Cannot build {} with a tree that has two top level nodes: {}".format(transition_scheme, tree))
 
-    for t in helper(tree.children[0]):
-        yield t
+#     for t in helper(tree.children[0]):
+#         yield t
 
-    yield Finalize(tree.label)
+#     yield Finalize(tree.label)
 
 def build_sequence(tree, transition_scheme=TransitionScheme.TOP_DOWN_UNARY):
     """
@@ -130,11 +131,11 @@ def build_sequence(tree, transition_scheme=TransitionScheme.TOP_DOWN_UNARY):
     """
     if transition_scheme is TransitionScheme.IN_ORDER:
         return list(yield_in_order_sequence(tree))
-    elif (transition_scheme is TransitionScheme.IN_ORDER_COMPOUND or
-          transition_scheme is TransitionScheme.IN_ORDER_UNARY):
-        return list(yield_in_order_compound_sequence(tree, transition_scheme))
-    else:
-        return list(yield_top_down_sequence(tree, transition_scheme))
+    # elif (transition_scheme is TransitionScheme.IN_ORDER_COMPOUND or
+    #       transition_scheme is TransitionScheme.IN_ORDER_UNARY):
+    #     return list(yield_in_order_compound_sequence(tree, transition_scheme))
+    # else:
+    #     return list(yield_top_down_sequence(tree, transition_scheme))
 
 def build_treebank(trees, transition_scheme=TransitionScheme.TOP_DOWN_UNARY, reverse=False):
     """
